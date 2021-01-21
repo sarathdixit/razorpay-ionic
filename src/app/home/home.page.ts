f626a8e23d6cf35f4995258552d68a0b790eac14
sarathdixit <sarathdixit29@gmail.com>
2021-01-21 15:18:39 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { AnonymousSubject } from 'rxjs/internal/Subject';
+
+declare var RazorpayCheckout:any;
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,5 +12,38 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  payWithRazorpay() {
+    var options = {
+      description: 'Credits towards consultation',
+      image: 'https://i.imgur.com/3g7nmJC.png',
+      currency: "INR", // your 3 letter currency code
+      key: "rzp_test_6PkxbyFtM91WyQ", // your Key Id from Razorpay dashboard
+      amount: 100, // Payment amount in smallest denomiation e.g. cents for USD
+      name: 'Razorpay',
+      prefill: {
+        email: 'test@razorpay.com',
+        contact: '9990009991',
+        name: 'Razorpay'
+      },
+      theme: {
+        color: '#F37254'
+      },
+      modal: {
+        ondismiss: function () {
+          alert('dismissed')
+        }
+      }
+    };
+
+    var successCallback = function (payment_id) {
+      alert('payment_id: ' + payment_id);
+    };
+
+    var cancelCallback = function (error) {
+      alert(error.description + ' (Error ' + error.code + ')');
+    };
+
+    RazorpayCheckout.open(options, successCallback, cancelCallback);
+  }
 
 }
